0fbf9008230975829eb9ce851d114c1744d3483a
Junior Alves <jrnalves@gmail.com>
2022-11-07 13:55:36 -0300

    add Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import Layout from '../components/Layout'
+
 import '../styles/globals.css'
 import "swiper/css/bundle"
 import '../styles/swiper.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
