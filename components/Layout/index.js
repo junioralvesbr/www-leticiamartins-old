23c61d71bc076e383ee0756423c25628c5a0cdb3
Junior Alves <jrnalves@gmail.com>
2022-11-07 15:20:10 -0300

    add whatsapp show render

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,16 +1,24 @@
 
+import { useState, useEffect } from 'react'
 import Footer from '../Footer'
 import WhatssApp from '../WhatsApp'
 
 export default function Layout({ children }) {
+  const [scrollTop, setScrollTop] = useState(0)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScrollTop(window.scrollY)
+    })
+  }, [])
+
   return (
     <>
       {children}
       <footer>
         <Footer />
       </footer>
-
-      {/* <WhatssApp /> */}
+      {scrollTop >= 50 && <WhatssApp />}
     </>
   )
 }
